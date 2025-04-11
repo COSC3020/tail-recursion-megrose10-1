8830da73f4bf9c91c67880e63a98d203e1263ca8
megrose10 <157438545+megrose10@users.noreply.github.com>
2025-04-10 23:08:48 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -1,26 +1,16 @@
 const fs = require('fs');
-const jsc = require('jsverify');
+const assert = require('assert');
 
-// Read in the target code (the tail recursive Fibonacci function)
-eval(fs.readFileSync('code.js') + '');  // Assuming the tail recursive `fib` is in 'code.js'
+eval(fs.readFileSync('code.js')+'');
 
-// Reference solution for Fibonacci using a simple iterative approach
-function fibTest(n) {
-    var fib_solns = [0];  // Fibonacci array initialized with 0 at index 0
-    if (n == 0) return fib_solns;  // Return the array for n=0
-    fib_solns[1] = 1;  // Fibonacci of 1 is 1
-    if (n == 1) return fib_solns;  // Return the array for n=1
-    for (var i = 2; i <= n; i++) {
-        fib_solns[i] = fib_solns[i-1] + fib_solns[i-2];  // Calculate Fibonacci values iteratively
-    }
-    return fib_solns[n];  // Return the Fibonacci value at index n
-}
+test1 = 4;
+assert(tsp_hk(test1) == 3);
 
-// JSVerify property test
-const test = jsc.forall("nat", function(n) {
-    // Compare the result of the tail-recursive Fibonacci with the iterative one
-    return JSON.stringify(fib(n)) === JSON.stringify(fibTest(n));
-});
+test2 = 7;
+assert(tsp_hk(test2) == 13);
 
-// Run the test and assert the result
-jsc.assert(test);
+test3 = 13; 
+assert(tsp_hk(test3) == 233);
+
+test4 = 64;
+assert(tsp_hk(test4) == 10610209857723)
