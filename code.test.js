4e48c31fe99652c42675574bf629898b1172b868
megrose10 <157438545+megrose10@users.noreply.github.com>
2025-04-10 23:12:08 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -4,13 +4,13 @@ const assert = require('assert');
 eval(fs.readFileSync('code.js')+'');
 
 test1 = 4;
-assert(tsp_hk(test1) == 3);
+assert(fib(test1) == 3);
 
 test2 = 7;
-assert(tsp_hk(test2) == 13);
+assert(fib(test2) == 13);
 
 test3 = 13; 
-assert(tsp_hk(test3) == 233);
+assert(fib(test3) == 233);
 
 test4 = 64;
-assert(tsp_hk(test4) == 10610209857723)
+assert(fib(test4) == 10610209857723)
