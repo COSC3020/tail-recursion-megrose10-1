b092506cfba042c61079dff8185dc91ae1f44985
megrose10 <157438545+megrose10@users.noreply.github.com>
2025-04-10 23:04:47 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,19 +1,20 @@
 function fib(n) {
     if(n == 0) {
-        return 1;
+        return 0;
     }
-    return fib_acc(n, 1, 1);
+    return fib_acc(n, 0, 1);
 }
 
 function fib_acc(n, accOne, accTwo) {
-    //console.log(accTwo);
-    if(n <= 2) {
+    //console.log("n: " + n);
+    //console.log("accTwo: " + accTwo);
+    if(n <= 1) {
         return accTwo;
     }
-    //console.log(accOne);
+    //console.log("accOne: " + accOne);
     return fib_acc(n - 1, accTwo, accOne + accTwo);
 }
 
-console.log(fib(4));
-console.log(fib(0));
-console.log(fib(7));
+//console.log(fib(4));
+//console.log(fib(0));
+//console.log(fib(7));
