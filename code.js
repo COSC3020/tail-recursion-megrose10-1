68833163bc4e4bd776697092a58f5bdb957cb007
megrose10 <157438545+megrose10@users.noreply.github.com>
2025-04-10 23:19:58 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,17 +1,21 @@
 function fib(n) {
+    //If n is 0, return 0
     if(n == 0) {
         return 0;
     }
+    //Recursive call must be last edit
     return fib_acc(n, 0, 1);
 }
 
 function fib_acc(n, accOne, accTwo) {
     //console.log("n: " + n);
     //console.log("accTwo: " + accTwo);
+    //When n is <= 1, return fib number
     if(n <= 1) {
         return accTwo;
     }
     //console.log("accOne: " + accOne);
+    //Calculate the next fib number by adding accOne and accTwo
     return fib_acc(n - 1, accTwo, accOne + accTwo);
 }
 
